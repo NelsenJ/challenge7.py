8c08f0efe4506802e6e273907469fd6a6902a90e
Nelsen <141732522+NelsenJ@users.noreply.github.com>
2024-02-03 10:46:41 +0700

    Update challenge7.py

--- challenge7.py
+++ challenge7.py
@@ -1,7 +1,8 @@
-x = int(input("Masukkan Bilangan : "))
+x = int(input("Masukkan Angka : "))
 while x >= 0:#biar looping :D
-    if  10 <= x >= 15 or 20 <= x >= 25 or 35 <= x >= 40:
+    if  10 <= x <= 15 or 20 <= x <= 25 or 35 <= x <= 40:
         print(" Benar...")
         break
     else:
         x = int(input("Masukkan Angka : "))
+        
